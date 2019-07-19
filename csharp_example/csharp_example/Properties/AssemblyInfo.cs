ceb7ecefc590779719513d30ab4e1a3547bb38fe
Сергей Зимацкий <ZimatskiiSN@ekassir.com>
2019-07-19 12:54:21 +0300

    css test

---
+++ csharp_example/csharp_example/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("csharp_example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("csharp_example")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2d09947-60a8-4a94-a18e-d2e34036aaf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp_example/csharp_example/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿ using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    [TestClass]
+    public class FirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private IWebElement element;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver("C:/Tools/");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTestTest ()
+        {
+            driver.Url = "https://www.pexels.com/";
+            Actions actions = new Actions(driver);
+            IWebElement search = driver.FindElement(By.CssSelector("html body header section div form div input")); // поиск по абсолютному пути
+            search.SendKeys("mountains");
+            driver.FindElement(By.CssSelector("button#search-action.rd__button")).Click(); // поиск по классу
+            driver.FindElement(By.CssSelector("article[data-meta-title='Silhouette Of Mountains · Free Stock Photo']")).Click(); // поиск конкретного эл-та по уникальному атрибуту
+            IWebElement dropdown = driver.FindElement(By.CssSelector("div[class='rd__dropdown rd__dropdown--right']")); //задаём элемент drop
+            actions.MoveToElement(dropdown).Build().Perform(); //наведение на элемент dropdown
+            driver.FindElement(By.CssSelector("input[value='640x411']")).Click();
+            driver.FindElement(By.CssSelector("button[class='js-download-custom-size-submit rd__button rd__button--full-width']")).Click();
+            Thread.Sleep(10000);
+            
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }        
+    }
+
+
+    /* Свалка
+     * 
+            driver.FindElement().SendKeys(Keys.ArrowDown);
+     **/
+}
+
+/* Свалка
+ * 
+            IWebElement test1 = driver.FindElement(By.XPath(".//*"));
+            driver.FindElement().SendKeys(Keys.ArrowDown);
+            test1.SendKeys(Keys.);
+            test1.SendKeys(Keys.ArrowDown);
+            test1.SendKeys(Keys.ArrowDown);
+            test1.SendKeys(Keys.Enter);
+            driver.FindElement(By.ClassName("HF9Klc ZYMsjf")).Click();
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google")); 
+
+
+ *
+ * */
